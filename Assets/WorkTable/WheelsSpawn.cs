2abc0c1530d664358fb5f905987d791e37dc065b
Maël <mael.perrot2005@gmail.com>
2024-04-25 07:16:08 +0200

    Update pour le 25/04

--- Assets/WorkTable/WheelsSpawn.cs
+++ Assets/WorkTable/WheelsSpawn.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class WheelsSpawn : MonoBehaviour
 {
+    public GameObject SelectedWheels;
+    public Transform Pos1;
+    public Transform Pos2;
+    public Transform Pos3;
+    public Transform Pos4;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,12 @@ public class WheelsSpawn : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnMouseDown()
+    {
+        Instantiate(SelectedWheels, Pos1);
+        Instantiate(SelectedWheels, Pos2);
+        Instantiate(SelectedWheels, Pos3);
+        Instantiate(SelectedWheels, Pos4);
     }
 }
